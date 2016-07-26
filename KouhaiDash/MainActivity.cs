b71a39b70c8de1c95b71eb6554b3b627b9feee76
Jia Yee <jiayeerawr@gmail.com>
2016-07-26 16:46:03 +0800

    Set up web view

---
+++ KouhaiDash/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace KouhaiDash
+{
+    [Activity(Label = "KouhaiDash!", MainLauncher = true, Theme = "@android:style/Theme.NoTitleBar", Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        WebView web_view;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.LoadUrl("http://kouhaidash.herokuapp.com");
+
+            web_view.SetWebViewClient(new HelloWebViewClient());
+        }
+
+        public class HelloWebViewClient : WebViewClient
+        {
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+                view.LoadUrl(url);
+                return true;
+            }
+        }
+
+        public override bool OnKeyDown(Android.Views.Keycode keyCode, Android.Views.KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && web_view.CanGoBack())
+            {
+                web_view.GoBack();
+                return true;
+            }
+
+            return base.OnKeyDown(keyCode, e);
+        }
+    }
+}
+
