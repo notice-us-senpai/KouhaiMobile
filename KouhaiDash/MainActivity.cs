1bd7a9474a56b171b507443be8c684e6c4582135
Jia Yee <jiayeerawr@gmail.com>
2016-07-29 00:01:34 +0800

    Add progress dialog aka preloader
    Others: Remove font family, update theme, etc.

--- KouhaiDash/MainActivity.cs
+++ KouhaiDash/MainActivity.cs
@@ -7,9 +7,12 @@ using Android.Widget;
 using Android.OS;
 using Android.Webkit;
 
+using System.Threading;
+
 namespace KouhaiDash
 {
-    [Activity(Label = "KouhaiDash!", MainLauncher = true, Theme = "@android:style/Theme.NoTitleBar", Icon = "@drawable/icon")]
+    [Activity(Label = "KouhaiDash!", MainLauncher = true, 
+        Icon = "@drawable/icon", Theme = "@android:style/Theme.Light.NoTitleBar")]
     public class MainActivity : Activity
     {
         WebView myWebView;
@@ -18,10 +21,20 @@ namespace KouhaiDash
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+
+            var progressDialog = ProgressDialog.Show(this, "Connecting to KouhaiServer", 
+                "Isn't it a beautiful day today?", true);
+
+            new Thread(new ThreadStart(() => {
+                Thread.Sleep(4 * 1000);
+                this.RunOnUiThread(() => {
+                    progressDialog.Dismiss();
+                });
+            })).Start();
+
             myWebView = FindViewById<WebView>(Resource.Id.MainWebView);
             myWebView.Settings.JavaScriptEnabled = true;
             myWebView.Settings.UseWideViewPort = true;
-            myWebView.Settings.StandardFontFamily = myWebView.Settings.SansSerifFontFamily;
             myWebView.LoadUrl("http://kouhaidash.herokuapp.com");
             myWebView.SetWebViewClient(new KouhaiWebViewClient());
         }
@@ -42,7 +55,6 @@ namespace KouhaiDash
                 myWebView.GoBack();
                 return true;
             }
-
             return base.OnKeyDown(keyCode, e);
         }
     }
